78bee371dd6a0275ff9e2fe00768efcc39177d48
seif <seenaseif@gmail.com>
2023-02-14 03:12:48 +0200

    first try to commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<>Hello React !</>);
+root.render(<>Hello React hello from seif !</>);
